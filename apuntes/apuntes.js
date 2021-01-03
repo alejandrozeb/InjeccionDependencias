512b98c228cf4c55dc01de539ecffbd7a3eb00e6
unknown <alejandrojzeballos@gmail.com>
2021-01-03 02:18:54 -0400

    inyeccion de dependencias introduccion

--- apuntes/apuntes.js
+++ apuntes/apuntes.js
@@ -1,6 +1,13 @@
 /* 
     -------------Inyeccion de dependencias--------
-    
+
+    Es un patron de diseño donde el objeto delega la construccion de sus dependencias a otra parte.
+    Es el acoplamiento entre clases, un objeto no deberia saber de la existencia de otro.
+    coupling y losecoupling
+
+    Mocking es un ejemplo de usar este patron.
+    Es delegar el control y cambiar el comportamiento de los objetos.
+
 
 
 */
